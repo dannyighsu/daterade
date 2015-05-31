cdbc81397417008310f44f2643cdde620d12b704
Danny Hsu <danny@kloudless.com>
2015-05-31 17:42:35 -0400

    Consolidated Maps API into 1 request, considerable speedup but limited to a small number of query results

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -14,43 +14,58 @@ class StaticPagesController < ApplicationController
   end
 
   def match
+    limit = 5
+    result_limit = 5
+
     # Find restaurant results
     input = params[:input]
     client = Yelp::Client.new({ consumer_key: ENV['Yelp_Key'], consumer_secret: ENV['Yelp_Secret'], token: ENV['Yelp_Token'], token_secret: ENV['Yelp_Token_Secret']})
-    searchParams = { term: 'dinner' }
+    searchParams = { term: 'dinner', limit: limit }
     restaurants = client.search(input, searchParams).businesses
 
     # Find attraction results
-    searchParams = { term: 'clubs or bars' }
+    searchParams = { term: 'clubs or bars', limit: limit }
     attractions = client.search(input, searchParams).businesses
 
     # Calculate results with Google Maps Distance Matrix API
-    output = 'json'
+    # construct query string
+    queryString = 'https://maps.googleapis.com/maps/api/distancematrix/json?'
+    origins = 'origins='
+    destinations = '&destinations='
+
+    restaurants.each do |r|
+      restaurant_addr = r.location.address[0] + ' ' + r.location.city + ', ' + r.location.state_code + ' ' + r.location.postal_code
+      restaurant_addr.gsub!(' ', '+')
+      origins = origins + restaurant_addr + '%7c'
+    end
+    origins = origins[0..-4]
+
+    attractions.each do |a|
+      attraction_addr = a.location.address[0] + ' ' + a.location.city + ', ' + a.location.state_code + ' ' + a.location.postal_code
+      attraction_addr.gsub!(' ', '+')
+      destinations = destinations + attraction_addr + '%7c'
+    end
+    destinations = destinations[0..-4]
+
+    # Make get request to Maps API and parse result
+    response = HTTParty.get(queryString + origins + destinations + '&key=' + ENV['Google_Key']).body
+    response = JSON.parse(response)
+    logger.info(response)
 
     location_hash = {}
     distances = []
-    restaurants.each do |r|
-
-      restaurant_addr = r.location.address[0] + ' ' + r.location.city + ', ' + r.location.state_code + ' ' + r.location.postal_code
-      restaurant_addr.gsub!(' ', '+')
-
-      attractions.each do |a|
-        attraction_addr = a.location.address[0] + ' ' + a.location.city + ', ' + a.location.state_code + ' ' + a.location.postal_code
-        attraction_addr.gsub!(' ', '+')
-
-        parameters = 'origins=' + restaurant_addr + '&destinations=' + attraction_addr
-        response = HTTParty.get('https://maps.googleapis.com/maps/api/distancematrix/' + output + '?' + parameters).body
-        response = JSON.parse(response)
-        distance = response["rows"][0]["elements"][0]["distance"]["value"]
-        distances.push(distance)
-        location_hash[distance] = [r, a]
-      end
+    for i in 0 ... limit * limit
+      distance = response["rows"][i / limit]["elements"][i % limit]["distance"]["value"]
+      distances.push(distance)
+      origin = restaurants[i / limit]
+      destination = attractions[i % limit]
+      location_hash[distance] = [origin, destination]
     end
 
     distances.sort
     @results = {}
 
-    for i in 0...7
+    for i in 0...result_limit
       @results[distances[i]] = JSON.parse(location_hash[distances[i]].to_json)
     end
 
